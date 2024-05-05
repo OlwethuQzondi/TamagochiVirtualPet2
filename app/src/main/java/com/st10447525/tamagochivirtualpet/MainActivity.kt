757301b0beaf184eee4b8ed76f61a2e102fcd9ec
OlwethuQzondi <164016147+OlwethuQzondi@users.noreply.github.com>
2024-05-05 16:16:42 +0200

    Initial commit of the Tamagochi Virtual Pet application build progress. Application still requires a few feature amendments.

--- app/src/main/java/com/st10447525/tamagochivirtualpet/MainActivity.kt
+++ app/src/main/java/com/st10447525/tamagochivirtualpet/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
+
 class MainActivity : AppCompatActivity() {
     //declaration
     private lateinit var textView: TextView
